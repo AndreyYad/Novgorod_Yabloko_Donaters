92afa20e088de2dada3650760ff544e77b807504
RubyRed <77550437+AndreyYad@users.noreply.github.com>
2024-02-19 20:05:47 +0300

    delete vk_api

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from asyncio import run
-from vk_api.bot_longpoll import VkBotLongPoll
 
 from bot.dispatcher import dp
 from bot.bot import bot
 from modules.register_handlers import register_handlers
 from modules.database import create_database
-from vk_bot.vk_bot import vk_session
-
-from modules.diagram import create_diagram
 
 async def main():
     await create_database()
